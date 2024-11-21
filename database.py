639cef94b0ee4d9353c8ebf1e6a369c8ec884ba2
seonhu <seonhu0810@gmail.com>
2024-11-21 10:22:27 +0900

    ãSQLAlchemy를 사용한 데이터베이스 연결 및 설정 추가

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 데이터베이스 URL 정의
+SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+
+# 데이터베이스와의 연결을 생성하는 engine 객체 생성
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# 데이터베이스 세션을 생성하는 팩토리 함수 SessionLocal을 정의
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 데이터베이스 모델의 베이스 클래스 생성
+Base = declarative_base()
